db49cc908ca070d89a4056e6c2cc7aaf9178c058
moin-khan-1 <moinkhan768mh@gmail.com>
2026-08-30 21:26:32 +0500

    Prepare backend for Render

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -22,6 +22,6 @@ app.get("/", (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
