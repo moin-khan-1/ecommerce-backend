266b542e936062b6fd4f47931c0ce288c28f704c
moin-khan-1 <moinkhan768mh@gmail.com>
2026-08-30 22:56:30 +0500

    Configure backend for Vercel

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ app.get("/", (req, res) => {
     });
 });
 
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
